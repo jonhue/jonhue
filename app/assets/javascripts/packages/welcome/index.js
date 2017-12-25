ef6f5a84cb1789638446c1789548bfe13be2a2ad
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-25 14:18:07 +0100

    foundation

---
+++ app/assets/javascripts/packages/welcome/index.js
@@ -0,0 +1,12 @@
+document.addEventListener( 'turbolinks:load', function() {
+    if ( $('body.welcome.index').length > 0 ) {
+        packagesWelcomeIndex();
+    };
+});
+
+
+
+
+function packagesWelcomeIndex() {
+    
+};
