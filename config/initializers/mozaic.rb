9dc4cdf74b9b20dbfe808003b97127bdc2984f5c
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-08 12:10:10 +0100

    update mozaic

--- config/initializers/mozaic.rb
+++ config/initializers/mozaic.rb
@@ -1,5 +1,12 @@
 Mozaic.configure do |config|
 
+    # Use Mozaic with Webpacker
+    # config.es6 = true
+    # Javascript asset directory
+    config.javascripts = 'app/webpack/javascripts'
+    # Stylesheet asset directory
+    config.stylesheets = 'app/webpack/stylesheets'
+
     # Define Mozaic components
     config.define_component 'layouts/google_analytics', tracking_id: Settings.google_analytics.tracking_id
 
