069acdae7e125442a74eec511ca45f13b381136b
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-18 18:11:06 +0100

    minor bugfixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'rss'
+Feature.destroy_all
 
 
 apps = JSON.parse(File.read('data/apps.json')).map { |h| h.deep_symbolize_keys }
