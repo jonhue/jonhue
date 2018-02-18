8ab6cf7223d21c947b420c1b7d7955b5fd0725f6
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-18 22:28:25 +0100

    add drawer

--- app/webpack/javascripts/myg/drawer.js
+++ app/webpack/javascripts/myg/drawer.js
@@ -1,5 +1,6 @@
 import MygDrawer from 'myg-drawer';
 
 document.addEventListener( 'turbolinks:load', () => {
-    // const mygDrawer = MygDrawer.init( document.querySelector('.myg-drawer'), {} );
+    const mygDrawer = MygDrawer.init( document.querySelector('.myg-drawer'), {} );
+    document.querySelector('.mdc-toolbar__section.mdc-toolbar__section--align-end.mobile > a > svg').addEventListener( 'click', () => mygDrawer.open() );
 })
--- app/webpack/javascripts/myg/toolbar.js
+++ app/webpack/javascripts/myg/toolbar.js
@@ -2,4 +2,14 @@ import MygToolbar from 'myg-toolbar';
 
 document.addEventListener( 'turbolinks:load', () => {
     const mygToolbar = MygToolbar.init( document.querySelector('.myg-toolbar'), {} );
+    let didScroll,
+        lastScrollTop = 0,
+        delta = 5;
+    window.addEventListener( 'scroll', () => didScroll = true );
+    setInterval(() => {
+        if (didScroll) {
+            lastScrollTop = mygToolbar.onScroll( lastScrollTop, delta );
+            didScroll = false;
+        };
+    }, 250);
 })
