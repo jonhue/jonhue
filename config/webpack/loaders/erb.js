0fc4094aec8183f9170ab5ec15e7c8a6de2b561c
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-02-18 12:43:17 +0100

    Minor bugfixes

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -5,7 +5,7 @@ module.exports = {
     use: [{
         loader: 'rails-erb-loader',
         options: {
-            runner: 'ruby bin\\rails runner'
+            runner: 'ruby bin/rails runner'
         }
     }]
 }
