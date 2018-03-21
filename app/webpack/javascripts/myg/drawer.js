d7f4b81f109f6d8511cb112f98a189403b166ced
Jonas Hübotter <me@jonhue.me>
2018-03-21 16:10:29 +0100

    improved styling

--- app/webpack/javascripts/myg/drawer.js
+++ app/webpack/javascripts/myg/drawer.js
@@ -1,8 +1,8 @@
 import MygDrawer from 'myg-drawer';
 
-document.addEventListener( 'turbolinks:load', () => {
-    document.querySelector('.mdc-toolbar__section.mdc-toolbar__section--align-end.mobile > a > svg').addEventListener( 'click', () => mygDrawer.open() );
-}
+// document.addEventListener( 'turbolinks:load', () => {
+//     document.querySelector('.mdc-toolbar__section.mdc-toolbar__section--align-end.mobile > a > svg').addEventListener( 'click', () => mygDrawer.open() );
+// }
 
 ['turbolinks:load', 'modalist:render', 'myg-tabs:render'].forEach( (event) => {
     document.removeEventListener( event, init );
