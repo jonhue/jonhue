ead61284cf2fec0e1d49c63739618a1b6ae0ca8c
Jonas Hübotter <me@jonhue.me>
2018-03-21 16:54:40 +0100

    update repository data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
         turbolinks_animate 'fadein'
         @apps = App.all.order('released_at desc')
         @repositories = Repository.all.order('updated_at desc')
-        @posts = Post.all.order(:created_at).limit(3)
+        @posts = Post.all.order(:created_at).limit(2)
     end
 
     def contact
