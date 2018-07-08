e681fe875ada76ce7c043e6ac3868d6fd1328d32
Jonas Hübotter <me@jonhue.me>
2018-07-08 11:49:07 +0200

    address eslint offenses

--- app/webpack/javascripts/myg/toolbar.js
+++ app/webpack/javascripts/myg/toolbar.js
@@ -7,13 +7,13 @@ import MygToolbar from 'myg-toolbar';
 function init() {
   const mygToolbar = MygToolbar.init(document.querySelector('.myg-toolbar'), {});
   let didScroll,
-      lastScrollTop = 0,
-      delta = 5;
+    lastScrollTop = 0,
+    delta = 5;
   window.addEventListener('scroll', () => didScroll = true);
   setInterval(() => {
     if (didScroll) {
       lastScrollTop = mygToolbar.onScroll(lastScrollTop, delta);
       didScroll = false;
-    };
+    }
   }, 250);
 }
